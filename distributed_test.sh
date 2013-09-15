ab16df013627515ff1cd4a01a5228542cabeb07c
Zach Reizner <zach297@gmail.com>
2013-09-15 12:59:03 -0500

    fix distributed_test.sh

--- distributed_test.sh
+++ distributed_test.sh
@@ -1,13 +1,14 @@
 trap 'kill 0' SIGINT SIGTERM EXIT
 
-LOG_BIN='~/cs425-fall13/main'
+LOG_BIN='./main'
 MACHINES="linux3.ews.illinois.edu:8886,linux4.ews.illinois.edu:7771,linux5.ews.illinois.edu:7772,linux6.ews.illinois.edu:7773"
-LOG_PREFIX="~/tmp/"
+LOG_PREFIX=""
+NETID=
 
-ssh -f jankaus2@linux3.ews.illinois.edu "$LOG_BIN --bind=\":8886\" -machines=$MACHINES -logs=\"${LOG_PREFIX}machine.0.log\" -batch"
+ssh ${NETID}@linux3.ews.illinois.edu "$LOG_BIN --bind=\":8886\" -machines=$MACHINES -logs=\"${LOG_PREFIX}machine.0.log\" -batch" &
 sleep 0.5 && echo
-ssh -f jankaus2@linux4.ews.illinois.edu "$LOG_BIN --bind=\":7771\" -machines=$MACHINES -logs=\"${LOG_PREFIX}machine.1.log\" -batch"
+ssh ${NETID}@linux4.ews.illinois.edu "$LOG_BIN --bind=\":7771\" -machines=$MACHINES -logs=\"${LOG_PREFIX}machine.1.log\" -batch" &
 sleep 0.5 && echo
-ssh -f jankaus2@linux5.ews.illinois.edu "$LOG_BIN --bind=\":7772\" -machines=$MACHINES -logs=\"${LOG_PREFIX}machine.2.log\" -batch"
+ssh ${NETID}@linux5.ews.illinois.edu "$LOG_BIN --bind=\":7772\" -machines=$MACHINES -logs=\"${LOG_PREFIX}machine.2.log\" -batch" &
 sleep 0.5 && echo
-ssh jankaus2@linux6.ews.illinois.edu "$LOG_BIN --bind=\":7773\" -machines=$MACHINES -logs=\"${LOG_PREFIX}machine.3.log\""
+ssh ${NETID}@linux6.ews.illinois.edu "$LOG_BIN --bind=\":7773\" -machines=$MACHINES -logs=\"${LOG_PREFIX}machine.3.log\""
